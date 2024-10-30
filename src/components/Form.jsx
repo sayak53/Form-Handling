798277282e11c2712cbea54a1c0511d141e08e8b
sayak53 <sayakchakraborty928@gmail.com>
2024-10-30 18:36:51 +0530

    first commit

---
+++ src/components/Form.jsx
@@ -0,0 +1,14 @@
+
+const Form = () => {
+    return (
+        <div>
+            <form>
+                <input type="text" placeholder="Enter your name"/>
+                <br />
+                <input type="submit" value="Submit"/>
+            </form>
+        </div>
+    );
+};
+
+export default Form;
