9b916b3fe01afdd0a2dffb3f8eb7f9445a63ac01
sayak53 <sayakchakraborty928@gmail.com>
2024-10-30 18:53:45 +0530

    learning basics about form handling

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,14 +1,24 @@
-
 const Form = () => {
-    return (
-        <div>
-            <form>
-                <input type="text" placeholder="Enter your name"/>
-                <br />
-                <input type="submit" value="Submit"/>
-            </form>
-        </div>
-    );
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target.name.value);
+    console.log(e.target.email.value)
+    console.log(e.target.password.value)
+    console.log("form submitted");
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="name" placeholder="Enter your name" />
+        <br />
+        <input type="email" name="email" placeholder="Enter your email" />
+        <br />
+        <input type="password" name="password" placeholder="Enter your password"/>
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
 };
 
 export default Form;
